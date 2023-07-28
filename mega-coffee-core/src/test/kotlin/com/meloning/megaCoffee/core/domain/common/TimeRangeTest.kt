b6e952baa0b4f66c738fb02bc1a77ab34df2960e
junsu <melon8372@gmail.com>
2023-07-28 20:52:57 +0900

    test: TimeRange overlaps 함수 테스트

--- mega-coffee-core/src/test/kotlin/com/meloning/megaCoffee/core/domain/common/TimeRangeTest.kt
+++ mega-coffee-core/src/test/kotlin/com/meloning/megaCoffee/core/domain/common/TimeRangeTest.kt
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import java.time.LocalTime
 
 class TimeRangeTest {
@@ -34,5 +36,25 @@ class TimeRangeTest {
             TimeRange(start, end)
         }.isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("종료 시간은 시작 시간보다 커야합니다.")
+    }
+
+    @ParameterizedTest
+    @DisplayName("타임 범위와 겹치는지 테스트")
+    @CsvSource(
+        "13:00, 20:00, 14:00, 19:00, TRUE",
+        "15:00, 20:00, 12:00, 14:00, FALSE",
+        "13:30, 20:50, 10:00, 13:30, TRUE",
+        "13:00, 20:00, 20:00, 21:00, TRUE",
+    )
+    fun overlapsTest(start: String, end: String, otherStart: String, otherEnd: String, expectedResult: Boolean) {
+        // given
+        val timeRange = TimeRange(LocalTime.parse(start), LocalTime.parse(end))
+        val other = TimeRange(LocalTime.parse(otherStart), LocalTime.parse(otherEnd))
+
+        // when
+        val result = timeRange.overlapsWith(other)
+
+        // then
+        assertThat(result).isEqualTo(expectedResult)
     }
 }
