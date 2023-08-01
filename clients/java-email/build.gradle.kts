d328d837d54c0830d28137cff221e6fc3df521c5
junsu <melon8372@gmail.com>
2023-08-01 18:12:19 +0900

    [#3] bugfix: Gradle 빌드시 빌드, 테스트 실패 이슈 해결

--- clients/java-email/build.gradle.kts
+++ clients/java-email/build.gradle.kts
@@ -1,5 +1,10 @@
 description = "clients:java-email module"
 
+tasks {
+    bootJar { enabled = false }
+    jar { enabled = true }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
 }
--- mega-coffee-infra/message-queue/rabbitmq/build.gradle.kts
+++ mega-coffee-infra/message-queue/rabbitmq/build.gradle.kts
@@ -1,5 +1,10 @@
 description = "mega-coffee-infra:message-queue:rabbitmq module"
 
+tasks {
+    bootJar { enabled = false }
+    jar { enabled = true }
+}
+
 dependencies {
     implementation(project(":mega-coffee-common"))
     compileOnly(project(":mega-coffee-core"))
