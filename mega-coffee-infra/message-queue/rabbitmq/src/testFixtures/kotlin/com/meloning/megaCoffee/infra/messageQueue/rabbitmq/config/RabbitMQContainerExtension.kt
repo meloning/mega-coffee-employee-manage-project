dafaebc7060374d6426d38fb24dec148c999eadb
junsu <melon8372@gmail.com>
2023-08-14 05:48:09 +0900

    [#1] bugfix: RabbitMQ ACCESS_REFUSED 에러 해결

--- mega-coffee-infra/message-queue/rabbitmq/src/testFixtures/kotlin/com/meloning/megaCoffee/infra/messageQueue/rabbitmq/config/RabbitMQContainerExtension.kt
+++ mega-coffee-infra/message-queue/rabbitmq/src/testFixtures/kotlin/com/meloning/megaCoffee/infra/messageQueue/rabbitmq/config/RabbitMQContainerExtension.kt
@@ -4,27 +4,26 @@ import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.Extension
 import org.junit.jupiter.api.extension.ExtensionContext
+import org.slf4j.LoggerFactory
 import org.testcontainers.containers.RabbitMQContainer
 import org.testcontainers.junit.jupiter.Container
 
-class RabbitMQContainerExtension : Extension, BeforeAllCallback, AfterAllCallback {
-    companion object {
-        private const val RABBITMQ_IMAGE_NAME = "rabbitmq:3-management"
+object RabbitMQContainerExtension : Extension, BeforeAllCallback, AfterAllCallback {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+    private const val RABBITMQ_IMAGE_NAME = "rabbitmq:3-management"
 
-        @JvmStatic
-        @Container
-        val rabbitmq: RabbitMQContainer = RabbitMQContainer(RABBITMQ_IMAGE_NAME)
+    @Container
+    val rabbitmq: RabbitMQContainer = RabbitMQContainer(RABBITMQ_IMAGE_NAME)
 
-        /**
-         * DynamicPropertySource가 동작하지 않아 System property 주입으로 해결
-         * @see https://stackoverflow.com/a/74121511
-         */
-        init {
-            System.setProperty("spring.rabbitmq.host", rabbitmq.host)
-            System.setProperty("spring.rabbitmq.port", rabbitmq.exposedPorts[0].toString())
-            System.setProperty("spring.rabbitmq.username", rabbitmq.adminUsername)
-            System.setProperty("spring.rabbitmq.password", rabbitmq.adminPassword)
-        }
+    /**
+     * DynamicPropertySource가 동작하지 않아 System property 주입으로 해결
+     * @see https://stackoverflow.com/a/74121511
+     */
+    private fun initProperty() {
+        System.setProperty("spring.rabbitmq.host", rabbitmq.host)
+        System.setProperty("spring.rabbitmq.port", rabbitmq.amqpPort.toString())
+        System.setProperty("spring.rabbitmq.username", rabbitmq.adminUsername)
+        System.setProperty("spring.rabbitmq.password", rabbitmq.adminUsername)
     }
 
     override fun beforeAll(context: ExtensionContext) {
@@ -32,6 +31,9 @@ class RabbitMQContainerExtension : Extension, BeforeAllCallback, AfterAllCallbac
             return
         }
         rabbitmq.start()
+        initProperty()
+
+        logger.info("[Test-RabbitMQ]\n" + rabbitmq.execInContainer("rabbitmqadmin", "list", "users").stdout)
     }
 
     override fun afterAll(context: ExtensionContext) {
